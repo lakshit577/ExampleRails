f10fb0fa43871179abdc9ab621ea4f3dbf271985
lakshit gupta <lakshitgupta218@gmail.com>
2024-02-23 12:14:53 +0530

    added course scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_22_061958) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_23_062636) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
